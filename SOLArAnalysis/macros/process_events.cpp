31a3fe8f0bc1d158388a4b6de0f0730b7512cd15
Dan Guff <daniele.guffanti@unimib.it>
2022-06-29 15:47:14 +0200

    added display function

--- SOLArAnalysis/macros/process_events.cpp
+++ SOLArAnalysis/macros/process_events.cpp
@@ -15,6 +15,7 @@
 #include "TF1.h"
 #include "TRandom3.h"
 #include "TGraph2D.h"
+#include "TTimer.h"
 
 #include "event/SLArMCEvent.hh"
 #include "event/SLArMCPrimaryInfo.hh"
@@ -28,6 +29,7 @@
 // FUNCTIONS FORWARD DEFINITIONS
 double view_event(SLArMCEvent* ev, bool do_draw = false);
 double q_diff_and_record(SLArMCEvent* ev, slarq::SLArQReadout* qev); 
+void   display(SLArMCEvent* ev, slarq::SLArQReadout* qev, TCanvas* cv); 
 
 
 int analyze_file(const char* path, bool do_draw = false) {
@@ -46,7 +48,7 @@ int analyze_file(const char* path, bool do_draw = false) {
   TTree* qtree = new TTree("qtree", "charge readout tree"); 
 
   slarq::SLArQReadout* ev_q = new slarq::SLArQReadout(0, "ev_q");
-  ev_q->SetReadoutAxis(slarq::kTime, 0, 800, 1.874, "Time [#mus]"); 
+  ev_q->SetReadoutAxis(slarq::kTime, 0, 400, 1.874, "Time [#mus]"); 
   ev_q->SetReadoutAxis(slarq::kX, 0., 1.2e3, 3.0, "#it{x} [mm]"); 
   ev_q->SetReadoutAxis(slarq::kY, 0., 2.3e3, 3.0, "#it{y} [mm]"); 
   ev_q->SetReadoutAxis(slarq::kZ, 0., 6.0e3, 3.0, "#it{z} [mm]"); 
@@ -54,7 +56,11 @@ int analyze_file(const char* path, bool do_draw = false) {
   ev_q->BuildHistograms();
   qtree->Branch("qreadout", &ev_q); 
 
-  for (int iev = 0; iev<1000; iev++) {
+  TCanvas* cTmp = new TCanvas("cTmp", "ev display", 0, 0, 800, 800); 
+  cTmp->Divide(2, 2); 
+  TTimer  *timer = new TTimer("gSystem->ProcessEvents();", 500, kFALSE);
+
+  for (int iev = 0; iev<500; iev++) {
     if (iev%20 == 0) {
       printf("processing ev %i (%.2f%%)\n", iev,  100*iev / (double)t->GetEntries()); 
     }
@@ -69,11 +75,20 @@ int analyze_file(const char* path, bool do_draw = false) {
     double qtot = q_diff_and_record(ev, ev_q); 
 
     //printf("qtot = %g - expected = %g [%g MeV]\n", qtot, te / W, te);
-    //ev_q->Clustering(); 
+    ev_q->Clustering(); 
+
+    timer->TurnOn(); 
+    timer->Reset(); 
+    display(ev, ev_q, cTmp); 
+    getchar(); 
+
+    timer->TurnOff(); 
 
     qtree->Fill(); 
 
     ev_q->ResetEvent(); 
+
+    printf("\n\n"); 
   }
 
   qtree->Write(); 
@@ -88,27 +103,29 @@ double q_diff_and_record(SLArMCEvent* ev, slarq::SLArQReadout* qev) {
 
   size_t ip = 0; 
   for (const auto &p : primaries) {
+    p->PrintParticle(); 
     double primary_edep = 0; 
     auto trajectories = p->GetTrajectories(); 
-    //printf("%lu associated trajectories\n", trajectories.size()); 
+    printf("%lu associated trajectories\n", trajectories.size()); 
     int itrj = 0;
 
     slarq::SLArQDiffusion diff; 
 
-    if (trajectories.size() > 0) {
-      for (const auto &trj : trajectories) {
-        //printf("trajectory %i has %lu points\n", itrj, trj->GetPoints().size()); 
-        if (trj->GetPoints().size() > 0) {
-          for (const auto &tp : trj->GetPoints()) {
-            double q = diff.DiffuseAnalytical(&tp,  qev); 
-            primary_edep += q;
-          }
+    for (const auto &trj : trajectories) {
+      //printf("trajectory %i has %lu points\n", itrj, trj->GetPoints().size()); 
+      if (trj->GetPoints().size() > 0 && 
+          !(trj->GetParticleName().Contains("nu"))) {
+        for (const auto &tp : trj->GetPoints()) {
+          double q = diff.DiffuseRandom(&tp,  qev); 
         }
+        primary_edep += trj->GetTotalEdep(); 
+      }
 
-        itrj++;
-      } // end of loop over trajectories
-    }
+      itrj++;
+    } // end of loop over trajectories
     q_ev_obs += primary_edep; 
+    printf("total energy loss in trajectories: %g MeV\n", primary_edep); 
+
     ip++;
 
   } // end of loop over primary particles
@@ -116,6 +133,72 @@ double q_diff_and_record(SLArMCEvent* ev, slarq::SLArQReadout* qev) {
   qev->ApplySuppressionAndQRec(100); 
 
   return q_ev_obs; 
+}
+
+void display(SLArMCEvent* ev, slarq::SLArQReadout* qev, TCanvas* cv) {
+  // get readout hist
+  TH2D* hqzx = (TH2D*)qev->GetHist(slarq::kZ); 
+  TH2D* hqyx = (TH2D*)qev->GetHist(slarq::kY); 
+
+
+  std::vector<TGraph*> gTz; 
+  std::vector<TGraph*> gTy; 
+  auto primaries = ev->GetPrimaries(); 
+  for (const auto &p : primaries) {
+    auto trajectories = p->GetTrajectories(); 
+    int itj = 0; 
+    for (const auto &t : trajectories) {
+      TString p_name = t->GetParticleName(); 
+      if (!p_name.Contains("nu")) {
+        TGraph* gz = new TGraph(t->GetPoints().size()); 
+        TGraph* gy = new TGraph(t->GetPoints().size()); 
+        size_t ipt = 0; 
+        for (const auto &point : t->GetPoints()) {
+          gz->SetPoint(ipt, point.fZ+slarq::xshift[2], 
+              (point.fX+slarq::xshift[0]) / Vdrift); 
+          gy->SetPoint(ipt, point.fY+slarq::xshift[1], 
+              (point.fX+slarq::xshift[0]) / Vdrift); 
+          ipt++;
+        }
+        gz->SetName(Form("gz_ev%i_%s_t%i", ev->GetEvNumber(), t->GetParticleName().Data(), itj)); 
+        gy->SetName(Form("gy_ev%i_%s_t%i", ev->GetEvNumber(), t->GetParticleName().Data(), itj)); 
+        gTz.push_back(gz); 
+        gTy.push_back(gy);
+        itj++; 
+      }
+    }
+  }
+
+  cv->cd(2); 
+  hqzx->Draw("col");
+  for (auto &g : gTz) g->Draw("l same"); 
+
+  cv->cd(3); 
+  hqyx->Draw("col"); 
+  for (auto &g : gTy) g->Draw("l same"); 
+
+  for (const auto &cluster : qev->GetClusters()) {
+    printf("cluster %lu: Q = %g,  %lu points\n", 
+        cluster->get_id(), cluster->get_charge(), cluster->get_points().size()); 
+  }
+
+  auto hclusters = qev->GetClusterHists(); 
+  int ih = 0; 
+  cv->cd(4); 
+  for (const auto &h3 : hclusters) {
+    if (ih == 0 ) h3->Draw("box"); 
+    else h3->Draw("box same"); 
+    ih++; 
+  }
+
+  cv->Modified(); 
+  cv->Update(); 
+  getchar(); 
+
+  for (auto &h3 : hclusters) delete h3; 
+  hclusters.clear(); 
+  for (auto &g : gTy) delete g; 
+  for (auto &g : gTz) delete g; 
 }
 
 int main(int argc, const char* argv[]) {
