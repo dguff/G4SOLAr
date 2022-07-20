c4b6a1924eb095045bf173e1e6fd57a563338a12
Dan Guff <daniele.guffanti@unimib.it>
2022-07-20 22:48:40 +0200

    Add TNamed initialization

--- G4SOLAr/include/config/SLArCfgBaseModule.hh
+++ G4SOLAr/include/config/SLArCfgBaseModule.hh
@@ -1,6 +1,6 @@
 /**
  * @author      : Daniele Guffanti (daniele.guffanti@mib.infn.it)
- * @file        : SLArCfgBaseModule
+ * @file        : SLArCfgBaseModule.hh
  * @created     : martedì lug 19, 2022 10:40:14 CEST
  */
 
@@ -8,8 +8,8 @@
 
 #define SLARCFGBASEMODULE_HH
 
-#include "TObject.h"
-#include "TPRegexp.h"
+#include <iostream>
+#include "TNamed.h"
 #include "TVector3.h"
 #include "TGraph.h"
 
@@ -18,35 +18,36 @@ class SLArCfgBaseModule : public TNamed {
     SLArCfgBaseModule(); 
     SLArCfgBaseModule(int idx); 
     SLArCfgBaseModule(int idx, float xc, float yc, float zc, float phi, float theta, float psi); 
+    SLArCfgBaseModule(const SLArCfgBaseModule& base); 
     virtual ~SLArCfgBaseModule(); 
 
-    virtual void DumpInfo() {}; 
-    int         GetIdx   ()                {return fIdx   ;}
-    void        SetIdx   (int idx)         {fIdx   = idx  ;}
-    float       GetX     ()                {return fX     ;}
-    void        SetX     (float x)         {fX     = x    ;}
-    float       GetY     ()                {return fY     ;}
-    void        SetY     (float y)         {fY     = y    ;}
-    float       GetZ     ()                {return fZ     ;}
-    void        SetZ     (float z)         {fZ     =  z   ;}
-    float       GetPhi   ()                {return fPhi ;}
-    void        SetPhi   (float phi)       {fPhi = phi;}
-    float       GetTheta ()                {return fTheta ;}
-    void        SetTheta (float theta)     {fTheta = theta;}
-    float       GetPsi   ()                {return fPsi ;}
-    void        SetPsi   (float psi)       {fPsi = psi;}
-    float       GetPhysX ()                {return fPhysX ;}
-    void        SetPhysX (float x)         {fPhysX = x    ;}
-    float       GetPhysY ()                {return fPhysY ;}
-    void        SetPhysY (float y)         {fPhysY = y    ;}
-    float       GetPhysZ ()                {return fPhysZ ;}
-    void        SetPhysZ (float z)         {fPhysZ =  z   ;}
-    int         GetBinIdx()                {return  fBin  ;}
-    void        SetBinIdx(int bin)         {fBin   =  bin ;}
-    TGraph*     GetGraphShape()            {return fGShape;}
-    void        SetNormal(double x, double y, double z);
-    void        SetNormal(TVector3 vN)     {fNormal = vN  ;}
-    TVector3    GetNormal()                {return fNormal;}
+    inline virtual void DumpInfo() {}; 
+    inline int GetIdx() {return fIdx;}
+    inline void SetIdx(int idx) {fIdx = idx;}
+    inline float GetX() {return fX;}
+    inline void SetX(float x) {fX = x;}
+    inline float GetY() {return fY;}
+    inline void SetY(float y) {fY = y;}
+    inline float GetZ() {return fZ;}
+    inline void SetZ(float z) {fZ =  z;}
+    inline float GetPhi() {return fPhi;}
+    inline void SetPhi(float phi) {fPhi = phi;}
+    inline float GetTheta() {return fTheta;}
+    inline void SetTheta(float theta) {fTheta = theta;}
+    inline float GetPsi() {return fPsi;}
+    inline void SetPsi(float psi) {fPsi = psi;}
+    inline float GetPhysX() {return fPhysX;}
+    inline void SetPhysX(float x) {fPhysX = x;}
+    inline float GetPhysY() {return fPhysY;}
+    inline void SetPhysY(float y) {fPhysY = y;}
+    inline float GetPhysZ() {return fPhysZ;}
+    inline void SetPhysZ(float z) {fPhysZ =  z;}
+    inline int GetBinIdx() {return  fBin;}
+    inline void SetBinIdx(int bin) {fBin =  bin;}
+    inline TGraph* GetGraphShape() {return fGShape;}
+    inline void SetNormal(double x, double y, double z) {fNormal = TVector3(x, y, z);};
+    inline void SetNormal(TVector3 vN) {fNormal = vN;}
+    inline TVector3 GetNormal() {return fNormal;}
 
   protected: 
     int                 fIdx   ; //!< Module index
--- G4SOLAr/src/config/SLArCfgBaseModule.cc
+++ G4SOLAr/src/config/SLArCfgBaseModule.cc
@@ -4,33 +4,26 @@
  * @created     : martedì lug 19, 2022 10:54:11 CEST
  */
 
-#include <iostream>
-#include <string>
-#include <fstream>
-
-#include "TSystemDirectory.h"
-#include "TList.h"
-#include "TRegexp.h"
-#include "TMath.h"
-
 #include "config/SLArCfgBaseModule.hh"
 
-SLArCfgBaseModule::SLArCfgBaseModule() : 
+ClassImp(SLArCfgBaseModule)
+
+SLArCfgBaseModule::SLArCfgBaseModule() : TNamed(), 
   fIdx(-1), fBin(0), fX(0.), fY(0.), fZ(0.), fPhi(0.), fTheta(0.), fPsi(0.), 
-  fPhysX(0.), fPhysY(0.), fPhysZ(0.),  
+  fPhysX(0.), fPhysY(0.), fPhysZ(0.), fNormal(0, 0, 1), 
   fGShape(nullptr)
 {}
 
-SLArCfgBaseModule::SLArCfgBaseModule(int idx) : 
+SLArCfgBaseModule::SLArCfgBaseModule(int idx) : TNamed(),
   fIdx(idx), fBin(0), fX(0.), fY(0.), fZ(0.), fPhi(0.), fTheta(0.), fPsi(0.), 
-  fPhysX(0.), fPhysY(0.), fPhysZ(0.),
+  fPhysX(0.), fPhysY(0.), fPhysZ(0.), fNormal(0, 0, 1),
   fGShape(nullptr)
 {}
 
 SLArCfgBaseModule::SLArCfgBaseModule(int idx, float xc, float yc, float zc, 
-             float phi, float theta, float psi) : 
+             float phi, float theta, float psi) : TNamed(), 
   fIdx(idx), fBin(0), fX(xc), fY(yc), fZ(zc), fPhi(phi), fTheta(theta), fPsi(psi), 
-  fPhysX(0.), fPhysY(0.), fPhysZ(0.), 
+  fPhysX(0.), fPhysY(0.), fPhysZ(0.), fNormal(0, 0, 1),
   fGShape(nullptr)
 {
   fIdx   = idx  ; 
@@ -42,14 +35,29 @@ SLArCfgBaseModule::SLArCfgBaseModule(int idx, float xc, float yc, float zc,
   fPsi   = psi  ;
 }
 
+SLArCfgBaseModule::SLArCfgBaseModule(const SLArCfgBaseModule& base) : 
+  SLArCfgBaseModule() 
+{
+    fIdx = base.fIdx;
+    fBin = base.fBin;
+    fX = base.fX;
+    fY = base.fY;
+    fZ = base.fZ;
+    fPhi = base.fPhi;
+    fTheta = base.fTheta;
+    fPsi = base.fPsi;
+    fPhysX = base.fPhysX;
+    fPhysY = base.fPhysY;
+    fPhysZ = base.fPhysZ;
+    fNormal = base.fNormal;
+    if (base.fGShape) {
+      fGShape = (TGraph*)base.fGShape->Clone();
+    }
+}
+
 SLArCfgBaseModule::~SLArCfgBaseModule()
 {
   if (fGShape) delete fGShape;
-}
-
-void SLArCfgBaseModule::SetNormal(double x, double y, double z)
-{
-  fNormal = TVector3(x, y, z);
 }
 
 
