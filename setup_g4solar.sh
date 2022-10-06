c5d8e0ca31cc5b9151c8906e08243a3456bde119
Daniele Guffanti <daniele.guffanti@mib.infn.it>
2022-10-06 10:36:34 -0500

    set Geant4 installation path on FNAL computing nodes

--- setup_g4solar.sh
+++ setup_g4solar.sh
@@ -18,6 +18,8 @@ printf "setup root v6_22_08d -qe20:p392:prof\n"
 setup root v6_22_08d -qe20:p392:prof
 printf "setup geant4 v4_11_0_p02a -qe20:prof:qt\n"
 setup geant4 v4_11_0_p02a -qe20:prof:qt
+# export the geant4 installation directory path tothe variable  GEANT4_DIR
+export GEANT4_DIR="/cvmfs/larsoft.opensciencegrid.org/products/geant4/v4_11_0_p02a/Linux64bit+3.10-2.17-e20-qt-prof/bin"
 printf "setup gsl v2_6a\n"
 setup gsl v2_6a
 printf "setup clhep v2_4_5_1a -qe20:prof"
