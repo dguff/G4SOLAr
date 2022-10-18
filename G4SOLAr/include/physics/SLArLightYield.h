08bc8d93f39aaf4c31db7263ec1aa56f4f41fad7
Dan Guff <daniele.guffanti@unimib.it>
2022-10-18 20:17:15 +0200

    fixed indent

--- G4SOLAr/include/physics/SLArLightYield.h
+++ G4SOLAr/include/physics/SLArLightYield.h
@@ -8,35 +8,40 @@
 
 class SLArLightYield{
 
-private:
-	// All used for LArQL/Q
-	const double k_Birks=0.0486; // Birks Constant
-	const double A_Birks=0.800; // Birks Constant
-	const double Wion = 23.6 ; // W-value for ionisation in eV/e-
-	const double Nex_Ni = 0.29; // Ratio of number of electrons to number of ions
-	const double Ni = 1.0e6/Wion; // Ionization density in LAr
-	const double pLAr = 1.30;  // Density Liquid Argon
-	const double alpha = 0.032; // Fit result for the Corr function
-	const double beta = 0.008258; // Fit result for the Corr function
-	const std::vector<double> chi_param = {2.151572666e-5, -3.988504, 1.38343421, 1.9919521e-6};
+  private:
+    // ************************************************************************
+    // LArQL Argon Scintillation Prediction PARAMETRIZATION
+    // https://lss.fnal.gov/archive/2022/conf/fermilab-conf-22-088-nd.pdf
+    // ************************************************************************
 
-	double QBirks(double dE, double dx, double E_field); // Default Birks Law
-	double Corr(double dE, double dx, double E_field); // Correction factor for Birks Law
-	double QChi(double dE, double dx, double E_field); // Chi2 fit to Birks Law Correction
-	double Qinf(); // Charge yield at an infinite electric field -
+    // All used for LArQL/Q
+    const double k_Birks=0.0486; // Birks Constant
+    const double A_Birks=0.800; // Birks Constant
+    const double Wion = 23.6 ; // W-value for ionisation in eV/e-
+    const double Nex_Ni = 0.29; // Ratio of number of electrons to number of ions
+    const double Ni = 1.0e6/Wion; // Ionization density in LAr
+    const double pLAr = 1.30;  // Density Liquid Argon
+    const double alpha = 0.032; // Fit result for the Corr function
+    const double beta = 0.008258; // Fit result for the Corr function
+    const std::vector<double> chi_param = {2.151572666e-5, -3.988504, 1.38343421, 1.9919521e-6};
 
-	//Set fixed light yield for Flat-example
-	double FlatLightYieldPerMeV = 24000.;
+    double QBirks(double dE, double dx, double E_field); // Default Birks Law
+    double Corr(double dE, double dx, double E_field); // Correction factor for Birks Law
+    double QChi(double dE, double dx, double E_field); // Chi2 fit to Birks Law Correction
+    double Qinf(); // Charge yield at an infinite electric field -
+
+    //Set fixed light yield for Flat-example
+    double FlatLightYieldPerMeV = 24000.;
 
 
-public:
-	SLArLightYield(); // Constructor
-	~SLArLightYield(); // Destructor
+  public:
+    SLArLightYield(); // Constructor
+    ~SLArLightYield(); // Destructor
 
-	// Light and charge yield functions
-	double LArQL(double energyDeposit, double stepWidth, double electricField);
-	double LArQQ(double energyDeposit, double stepWidth, double electricField);
-	double Flat();
+    // Light and charge yield functions
+    double LArQL(double energyDeposit, double stepWidth, double electricField);
+    double LArQQ(double energyDeposit, double stepWidth, double electricField);
+    double Flat();
 
 };
 #endif
