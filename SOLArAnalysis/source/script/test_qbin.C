ebc02308174e7bd1db0ab3b5559a912dfb8264af
Dan Guff <daniele.guffanti@unimib.it>
2023-01-10 17:41:03 +0100

    Updated charge event display macro

--- SOLArAnalysis/source/script/test_qbin.C
+++ SOLArAnalysis/source/script/test_qbin.C
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include "TFile.h"
+#include "TROOT.h"
 #include "TTree.h"
 #include "TSystem.h"
 #include "TStyle.h"
@@ -24,6 +25,8 @@
 #include "event/SLArEventMegatile.hh"
 #include "event/SLArEventTile.hh"
 
+
+
 void test_qbin(const char* input_path, int iev = 0) 
 {
   TFile* file = new TFile(input_path); 
@@ -36,36 +39,48 @@ void test_qbin(const char* input_path, int iev = 0)
   double hmax = 0.;
 
   // read events
+  std::vector<TH2Poly*> pixMaps;
+  pixMaps.reserve(200); 
+
   auto anodeEv = ev->GetReadoutTileSystem(); 
+
   for (const auto& mtEv : anodeEv->GetMegaTilesMap()) {
+    auto mt_hits = mtEv.second->GetNChargeHits(); 
+    if (mt_hits == 0) continue;
+
     for (const auto& tEv : mtEv.second->GetTileMap()) {
+      auto t_hits = tEv.second->GetPixelHits();
+      if (t_hits == 0) continue;
+
+      TH2Poly* h2Map = anodeCfg->ConstructPixHistMap(mtEv.first, tEv.first);
+
       for (const auto& pix : tEv.second->GetPixelEvents()) {
         double nhits = pix.second->GetNhits(); 
         if (nhits > hmax) hmax = nhits;
-        anodeCfg->GetMap()[mtEv.first]->GetMap()[tEv.first]->GetPixHistMap()->SetBinContent(pix.first, nhits); 
+        h2Map->SetBinContent(pix.first, nhits); 
+        //anodeCfg->GetMap()[mtEv.first]->GetMap()[tEv.first]->GetPixHistMap()->SetBinContent(pix.first, nhits); 
       }
+      printf("charge map pointer %p\n", static_cast<void*>(h2Map));
+      printf("Hist name: %s\n", h2Map->GetName());
+
+      pixMaps.push_back(h2Map); 
     }
   }
 
   // Draw pixel hit-map
   TH2D* h = new TH2D("hFrame", "Anode readout;#it{z} [mm];#it{x} [mm]", 
-      100, -3e3, +3e3, 100, -2e3, 2e3); 
+      100, -6e3, +6e3, 100, -3e3, 3e3); 
 
   TCanvas* cP = new TCanvas("cP"); 
   cP->cd(); h->Draw("axis"); 
 
   bool is_first = true; 
-  for (const auto& mtCfg : anodeCfg->GetMap()) {
-    mtCfg.second->GetTH2()->Draw("same"); 
-    for (const auto& tCfg : mtCfg.second->GetMap()) {
-      tCfg.second->GetPixHistMap()->GetZaxis()->SetRangeUser(0., 1.05*hmax); 
-      if (is_first) {
-        tCfg.second->GetPixHistMap()->DrawClone("same colz0"); 
-        is_first = false; 
-      } else {
-        tCfg.second->GetPixHistMap()->DrawClone("same col0"); 
-      }
-    }
+  for (size_t j=0; j<pixMaps.size(); j++) {
+    auto qtile = pixMaps.at(j); 
+    printf("qtile ptr = %p\n", static_cast<void*>(qtile));
+    qtile->GetZaxis()->SetRangeUser(0, 1.05*hmax); 
+    if (is_first) {qtile->Draw("colz0 same"); is_first = false;}
+    else qtile->Draw("col0 same"); 
   }
 
   // Draw electron tracks
