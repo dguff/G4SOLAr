1c27e5e0c30bca8898a4f0bdd34a5cf2806966eb
Dan Guff <daniele.guffanti@unimib.it>
2023-02-23 14:59:22 +0100

    Renaming of IonAndScint class

--- G4SOLAr/src/physics/SLArIonAndScint.cc
+++ G4SOLAr/src/physics/SLArIonAndScint.cc
@@ -1,18 +1,18 @@
-#include "SLArLightYield.h"
+#include "SLArIonAndScint.h"
 #include <iostream>
 
 
-SLArLightYield::SLArLightYield()
+SLArIonAndScint::SLArIonAndScint()
 {
   // Default constructor
 }
-SLArLightYield::~SLArLightYield()
+SLArIonAndScint::~SLArIonAndScint()
 {
   // Default destructor
 }
 
 
-double SLArLightYield::QBirks(double dE, double dx, double E){
+double SLArIonAndScint::QBirks(double dE, double dx, double E) const {
     double dEdx = dE/dx;
     if(dEdx<0.1) dEdx = 0.1;
     double nom = A_Birks/Wion;
@@ -20,33 +20,33 @@ double SLArLightYield::QBirks(double dE, double dx, double E){
     return nom/denom*1.0e6;
 }
 
-double SLArLightYield::Corr(double dE, double dx, double E){
+double SLArIonAndScint::Corr(double dE, double dx, double E) const {
     double dEdx = dE/dx;
     if(dEdx<1) dEdx = 1.;
     return exp(-E/(alpha *log(dEdx) + beta));
 }
 
-double SLArLightYield::QChi(double dE, double dx, double E){
+double SLArIonAndScint::QChi(double dE, double dx, double E) const {
     double dEdx = dE/dx;
     if(dEdx<1) dEdx = 1.;
     return chi_param[0]/(chi_param[1]+exp(chi_param[2] + chi_param[3]*dEdx));
 }
 
-double SLArLightYield::Qinf(){
+double SLArIonAndScint::Qinf() const {
     return 1.0e6/Wion;
 }
 
-double SLArLightYield::LArQL(const double energy_deposit, const double hit_distance, const double electric_field){
+double SLArIonAndScint::LArQL(const double energy_deposit, const double hit_distance, const double electric_field) const {
     return Nex_Ni * Ni + Ni - LArQQ(energy_deposit,hit_distance, electric_field);
 }
 
-double SLArLightYield::LArQQ(const double energy_deposit, const double hit_distance, const double electric_field){
+double SLArIonAndScint::LArQQ(const double energy_deposit, const double hit_distance, const double electric_field) const {
   double QB = QBirks(energy_deposit, hit_distance, electric_field); 
   double QX = Corr(energy_deposit, hit_distance, electric_field)*
       QChi(energy_deposit, hit_distance, electric_field)*Qinf();
   return (QB + QX); 
 }
 
-double SLArLightYield::Flat(){
+double SLArIonAndScint::Flat() const {
     return FlatLightYieldPerMeV;
 }
